f7ae0ea9d8014324c5933307d61e9be512262982
naomijoyce <naomijoyceb@gmail.com>
2018-10-31 16:05:15 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,81 @@
+require "pry"
 # your code goes here
+class Person
+  attr_reader :name
+  attr_accessor :bank_account
+
+  def initialize(name, bank_account =25, happiness = 8, hygiene = 8)
+    @name = name
+    @bank_account = bank_account
+    @happiness = happiness
+    @hygiene = hygiene
+  end
+
+  def happiness=(happiness)
+    @happiness = happiness
+    @happiness = 10 if @happiness > 10
+    @happiness = 0 if @happiness < 0
+  end
+
+  def happiness
+    @happiness
+  end
+
+  def hygiene=(hygiene)
+    @hygiene = hygiene
+    @hygiene = 10 if @hygiene > 10
+    @hygiene = 0 if @hygiene < 0
+  end
+
+  def hygiene
+    @hygiene
+  end
+
+  def clean?
+    @hygiene > 7 ? true : false
+  end
+
+  def happy?
+    @happiness > 7 ? true : false
+  end
+
+  def get_paid(salary)
+    @bank_account += salary
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene=@hygiene + 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.happiness= @happiness + 2
+    self.hygiene = @hygiene - 3
+    "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    self.happiness= @happiness + 3
+    friend.happiness += 3
+    "Hi #{friend.name}! It's #{@name}. How are you?"
+  end
+
+  def start_conversation(person, topic)
+    case topic
+    when "politics"
+      self.happiness =@happiness - 2
+      person.happiness -= 2
+      "blah blah partisan blah lobbyist"
+    when "weather"
+      self.happiness = @happiness + 1
+      person.happiness += 1
+      "blah blah sun blah rain"
+    else
+      "blah blah blah blah blah"
+    end
+
+  end
+
+
+end
